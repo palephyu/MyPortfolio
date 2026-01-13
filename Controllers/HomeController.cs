cb6a9421a8dbae125256156c9fdb98fc403702b5
palephyu <palephyu12345@gmail.com>
2026-01-13 22:56:46 +0630

    my first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() => View();
+        public IActionResult About() => View();
+        public IActionResult Projects() => View();
+        public IActionResult Contact() => View();
+    }
+}
